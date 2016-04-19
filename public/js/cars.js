f8f1f20962554c89ec7622a720a5b982437db93a
jjlippman <jjlippman@earthlink.net>
2016-04-19 12:59:11 -0600

    Done.

--- public/js/cars.js
+++ public/js/cars.js
@@ -3,7 +3,7 @@
 // this is the base API url
 var baseUrl = "http://mimeocarlisting.azurewebsites.net/api/cars/";
 
-function formatCars(carsJSON) {
+function formatCars (carsJSON) {
   // this function shold return a string of properly formatted html
   // refer to app/views/index.erb lines 16 - 22 for an example of how
   // to format three cars, each in a div with a class "col-md-4", in a 
