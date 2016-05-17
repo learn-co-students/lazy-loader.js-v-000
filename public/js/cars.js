333f796d325cce744053e1ae9d8aea506a8a4339
evanscloud <dijkstrascloud@gmail.com>
2016-05-17 13:21:25 -0400

    Fix html variable to work properly

--- public/js/cars.js
+++ public/js/cars.js
@@ -4,34 +4,34 @@ var baseUrl = "http://mimeocarlisting.azurewebsites.net/api/cars/";
 var pageNumber = 3;
 
 function formatCars(carsJSON) {
+  var html = '<div class="row">';
+
   carsJSON.forEach(function(car){
     html += '<div class="col-md-4 car">';
     html += '<h2>' + car.Make + '</h2>';
     html += '<p><strong>Model:</strong> ' + car.Model + '</p>';
     html += '<p><strong>Year:</strong> ' + car.Year + '</p>';
-    html += '</div>'
+    html += '</div>';
   });
   html += '</div>';
   return html;
 }
 
 function addCarsToDOM(carsJSON) {
-  var formatData = formatCars(carsJSON);
-  $('#cars').append(formatData);
+  $('#cars').append(formatCars(carsJSON));
 }
 
 function fetchJSON() {
-  var url = baseUrl + page + "/3";
-  page += 1;
+  var url = baseUrl + pageNumber + "/3";
+  pageNumber += 1;
   $.ajax({
     url: url,
     contentType: 'application/json',
     dataType:'jsonp',
-    success:function(data){
+    success: function(data){
       addCarsToDOM(data);
-    },
-    error:function(error){
-      alert("Sorry, something went wrong: " + error.statusText);
     }
+  }).fail (function(error){
+    $('div#cars').alert("Sorry, something went wrong: " + error.statusText)
   });
 }
