f0bb8845a791fdc633dd2c1585a2a9d85eda7178
glee38 <glee3388@gmail.com>
2016-08-19 23:49:10 -0400

    lazy loader working

--- public/js/cars.js
+++ public/js/cars.js
@@ -3,20 +3,51 @@
 // this is the base API url
 var baseUrl = "http://mimeocarlisting.azurewebsites.net/api/cars/";
 
-function formatCars(carsJSON) {
   // this function shold return a string of properly formatted html
   // refer to app/views/index.erb lines 16 - 22 for an example of how
   // to format three cars, each in a div with a class "col-md-4", in a 
   // div with a class "row"
+
+function formatCars(carsJSON) {
+  const cars = carsJSON;
+  var html = "<div class=\"row\">";
+  
+  $.each(cars, function(index, car) {
+    html += "<div class=\"col-md-4 car\">";
+    html += "<h2>" + car.Make + "</h2>";
+    html += "<p><strong>Model:</strong> " + car.Model + "</p>";
+    html += "<p><strong>Year:</strong> " + car.Year + "</p>";
+    html += "</div>";
+  });
+
+  html += "</div>";
+  return html;
 }
 
-function addCarsToDOM(carsJSON) {
   // this function should pass carsJSON to formatCars() and then 
   // add the resulting HTML to the div with an id of "cars"
+
+function addCarsToDOM(carsJSON) {
+  var newHtml = formatCars(carsJSON);
+  $("#cars").append(newHtml);
 }
 
-function fetchJSON() {
   // this function will make the ajax call
   // on success of the ajax call, it will pass the returned data
   // to addCarsToDOM()
+  
+var pageNumber = 2;  
+
+function fetchJSON() {
+  pageNumber += 1;
+  const url  = baseUrl + pageNumber + "/3";
+  
+  $.ajax({
+    url: url,
+    contentType: 'application/json',
+    dataType: 'jsonp',
+    success: function(data) {
+      addCarsToDOM(data);
+    }
+  });
 }
