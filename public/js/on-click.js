8ec5f9d0f6fdd525ac42f91a27846f5dec26a636
Adam Taitano <adam.taitano@gmail.com>
2016-09-12 04:49:27 +0000

    Done.

--- public/js/on-click.js
+++ public/js/on-click.js
@@ -1,15 +1,5 @@
 "use strict";
 
 $(document).ready(function() {
-  $.ajax({
-       url: url,
-       contentType: 'application/json',
-       dataType: 'jsonp',
-       success: function(data) {
-         var price = data["LastPrice"];
-         $("#netflix-price").html("$" + price);
-       }
-     });
-  // add click listener here
-  // it should call on fetchJSON()
+  $('#load-cars').on('click', fetchJSON)
 });
