605a34fd69e82f00cf6d25d40fab6708351f2d56
Adam Taitano <adam.taitano@gmail.com>
2016-09-12 04:24:38 +0000

    Done.

--- public/js/on-click.js
+++ public/js/on-click.js
@@ -1,6 +1,15 @@
 "use strict";
 
 $(document).ready(function() {
+  $.ajax({
+       url: url,
+       contentType: 'application/json',
+       dataType: 'jsonp',
+       success: function(data) {
+         var price = data["LastPrice"];
+         $("#netflix-price").html("$" + price);
+       }
+     });
   // add click listener here
   // it should call on fetchJSON()
 });
