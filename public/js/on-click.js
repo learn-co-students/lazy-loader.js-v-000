c1b9d8ad6d2663aaee4c8ac3cad7cfd6b37e46f8
Caleb Faught <faughtcj@gmail.com>
2016-11-11 19:55:12 -0600

    Done.

--- public/js/on-click.js
+++ public/js/on-click.js
@@ -1,6 +1,10 @@
 "use strict";
 
 $(document).ready(function() {
+
   // add click listener here
   // it should call on fetchJSON()
+  $('#load-cars').on('click', function(event) {
+    fetchJSON();
+  });
 });
